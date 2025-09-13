41a2d6bd2af2a4c1a53c6b856a9379dad4e31b3f
Jay <jaylim95@hotmail.com>
2025-09-13 15:41:33 -0700

    testing out inngest

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,8 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
 
-const globalForPrisma = global as unknown as { 
-    prisma: PrismaClient
-}
+const prisma = new PrismaClient()
 
-const prisma = globalForPrisma.prisma || new PrismaClient()
+const globalForPrisma = global as unknown as { prisma: typeof prisma }
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
